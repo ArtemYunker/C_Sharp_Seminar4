62ed24635d8723cd4b1978d4c50dfd4af5278be0
Artem Yunker <art-yunker@yandex.ru>
2022-09-11 13:36:33 +0200

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+//  и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число B");
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+while (number > 0)
+{
+    int digit = number % 10; 
+    sum = digit + sum;
+    number = number / 10;   
+
+}
+Console.WriteLine(sum);
